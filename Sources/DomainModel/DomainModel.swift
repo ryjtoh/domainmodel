179798f1bc8ad7ada9afa464cf79e55d5f79e8ee
ryjtoh <60204869+ryjtoh@users.noreply.github.com>
2022-04-14 13:06:11 -0700

    finished money struct

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,15 +9,79 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    var amount: Int
+    var currency: String
+    
+    init(amount amt: Int, currency curr: String) {
+        self.amount = amt
+        self.currency = curr
+    }
+    
+    func convert(_ name: String) -> Money {
+        var currAmount = self.amount
+        
+        // Convert to USD first
+        if (self.currency == "GBP") {
+            currAmount *= 2
+        } else if (self.currency == "EUR") {
+            currAmount *= 2
+            currAmount /= 3
+        } else if (self.currency == "CAN") {
+            currAmount *= 4
+            currAmount /= 5
+        }
+        
+        // Convert to directed currency
+        if (name == "GBP") {
+            currAmount /= 2
+        } else if (name == "EUR") {
+            currAmount /= 2
+            currAmount *= 3
+        } else if (name == "CAN") {
+            currAmount /= 4
+            currAmount *= 5
+        }
+        
+        return Money(amount:currAmount, currency:name)
+       
+    }
+    
+    func add(_ money: Money) -> Money{
+        let converted = money.convert(self.currency).amount
+        
+        return Money(amount:converted + self.amount, currency: self.currency)
+    }
+    
+    func subtract(_ money: Money) -> Money{
+        let converted = money.convert(self.currency).amount
+        
+        return Money(amount:self.amount - converted, currency: self.currency)
+    }
 }
 
 ////////////////////////////////////
 // Job
 //
 public class Job {
+    var title: String
+    var type: JobType
+    
+    init(title:String, type: JobType) {
+        self.title = title
+        self.type = type
+    }
+    
     public enum JobType {
         case Hourly(Double)
         case Salary(UInt)
+    }
+    
+    func calculateIncome() -> Int {
+        var amount = self.type
+        type.self
+        if (amount is Double) {
+            
+        }
     }
 }
 
