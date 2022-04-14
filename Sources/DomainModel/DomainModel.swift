772d6c59251fe0ffd8656c7304b1e18ba55864db
ryjtoh <60204869+ryjtoh@users.noreply.github.com>
2022-04-14 16:19:02 -0700

    finished code without testing

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -76,11 +76,31 @@ public class Job {
         case Salary(UInt)
     }
     
-    func calculateIncome() -> Int {
-        var amount = self.type
-        type.self
-        if (amount is Double) {
-            
+    func calculateIncome(_ hours: Int) -> Int {
+        switch self.type {
+        case .Hourly(let value) :
+            return Int(value * Double(hours))
+        case .Salary(let value) :
+            return Int(value)
+        }
+    }
+    
+    func raise(byAmount: Double) {
+        switch self.type {
+        case .Hourly(let value):
+            self.type = JobType.Hourly(value + byAmount)
+        case .Salary(let value):
+            self.type = JobType.Salary(UInt(Double(value) + byAmount))
+        }
+    }
+    
+    func raise(byPercent: Double) {
+        let decimal = (byPercent / 100.0) + 1.0
+        switch self.type {
+        case .Hourly(let value):
+            self.type = JobType.Hourly(value * decimal)
+        case .Salary(let value):
+            self.type = JobType.Salary(UInt(Double(value) * decimal))
         }
     }
 }
@@ -89,10 +109,58 @@ public class Job {
 // Person
 //
 public class Person {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    var job: Job?
+    var spouse: Person?
+    var hasSpouse: Bool
+    
+    init(firstName: String, lastName: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.hasSpouse = false
+    }
+    
+    func toString() -> String {
+        return "Person: firstName: \(self.firstName) lastName: \(self.lastName) age: \(self.age) job: \(String(describing: self.job)) spouse: \(String(describing: self.spouse?.firstName))"
+    }
 }
 
-////////////////////////////////////
+//
 // Family
 //
 public class Family {
+    var members: [Person] = []
+    
+    init(spouse1: Person, spouse2: Person) {
+        if (spouse1.hasSpouse == false && spouse2.hasSpouse == false) {
+            spouse1.spouse = spouse2
+            spouse2.spouse = spouse1
+            spouse1.hasSpouse = true
+            spouse2.hasSpouse = true
+            self.members.append(spouse1)
+            self.members.append(spouse2)
+        }
+    }
+    
+    func haveChild(_ child: Person) -> Bool {
+        if (self.members[0].age > 21 || self.members[1].age > 21) {
+            self.members.append(child)
+            return true
+        }
+        return false
+    }
+    
+    func householdIncome() -> Int {
+        var total = 0
+        if (self.members[0].job != nil) {
+            total += (self.members[0].job?.calculateIncome(2000))!
+        }
+        if (self.members[1].job != nil) {
+            total += (self.members[1].job?.calculateIncome(2000))!
+        }
+        return total
+    }
 }
